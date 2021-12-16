807ce000296bfc68e92d14041f92ae35b62b044d
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2021-12-16 17:40:55 -0500

    update

---
+++ src/cons.js
@@ -0,0 +1,25 @@
+const proxy = "https://proxy-sites.herokuapp.com/";
+
+const PRE = "https://precio-site.herokuapp.com/api/v1/servicio/precio/v2/SITE";
+
+const WS = "0x0000000000000000000000000000000000000000";//0x0000000000000000000000000000000000000000 recibe los huerfanos por defecto
+
+var SCtest = "0xEC836b3F0fa7BF1579ab32C57A4e7dee971ccDcE";// direccion del contrato de pruebas test only no real
+var SC = "0xaD356cA07BE4A9a3237b7d4326ad07D08846fb60";// direccion del contrato V1
+var SC2 = "0xaD356cA07BE4A9a3237b7d4326ad07D08846fb60";// direccion del contrato V2
+
+var TOKEN = "0x55d398326f99059fF775485246999027B3197955";
+
+
+if(true){// testnet comand
+
+    SCtest = "0xab56a7c0211a7a187aa9Fda096A092dCE30F2D22";// direccion del contrato de pruebas test only no real
+    SC = "0xab56a7c0211a7a187aa9Fda096A092dCE30F2D22";// direccion del contrato V1
+    SC2 = "0xab56a7c0211a7a187aa9Fda096A092dCE30F2D22";// direccion del contrato V2
+
+    TOKEN = "0xd5881b890b443be0c609BDFAdE3D8cE886cF9BAc";
+
+}
+
+
+export default {proxy, WS, SCtest, SC, SC2, PRE, TOKEN};
