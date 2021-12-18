1c59963d6195bfc6ffaa2e9102af60369554b24b
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2021-12-18 18:43:17 -0500

    Revert "h"
    This reverts commit ca1448bdf1bca0e4c256aa4ced001cd16505545c.

--- src/cons.js
+++ src/cons.js
@@ -5,8 +5,8 @@ const PRE = "https://precio-site.herokuapp.com/api/v1/servicio/precio/v2/SITE";
 const WS = "0x0000000000000000000000000000000000000000";//0x0000000000000000000000000000000000000000 recibe los huerfanos por defecto
 
 var SCtest = "0xEC836b3F0fa7BF1579ab32C57A4e7dee971ccDcE";// direccion del contrato de pruebas test only no real
-var SC = "0xBF0F98912d16b8D42410c89282aa663d8C8db8fB";// direccion del contrato V1
-var SC2 = "0xBF0F98912d16b8D42410c89282aa663d8C8db8fB";// direccion del contrato V2
+var SC = "0xaD356cA07BE4A9a3237b7d4326ad07D08846fb60";// direccion del contrato V1
+var SC2 = "0xaD356cA07BE4A9a3237b7d4326ad07D08846fb60";// direccion del contrato V2
 
 var TOKEN = "0x55d398326f99059fF775485246999027B3197955";
 
