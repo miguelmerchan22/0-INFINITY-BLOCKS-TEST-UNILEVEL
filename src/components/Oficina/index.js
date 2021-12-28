e58abc95ce8c4b6234410ef9278f49191cd383e3
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2021-12-27 19:11:27 -0500

    update infinity

--- src/components/Oficina/index.js
+++ src/components/Oficina/index.js
@@ -434,7 +434,7 @@ export default class Oficina extends Component {
               
             <div id="card-stats" className="pt-0">
                 <div className="row">
-                  <div className="col s12 m6 l6 xl3">
+                  <div className="col s12 m6 l6 xl6">
                     <div className="card gradient-45deg-light-blue-cyan gradient-shadow min-height-100 white-text animate fadeLeft">
                       <div className="padding-4">
                         <div className="row">
@@ -445,7 +445,7 @@ export default class Oficina extends Component {
                           </div>
                           <div className="col s8 m8 right-align ">
                             <h5 className="mb-0 white-text">
-                              {invested / 50} BLKS
+                              {invested / 50} BLOCKS
                             </h5>
                             <p className="no-margin">$ {invested}</p>
                           </div>
@@ -457,7 +457,7 @@ export default class Oficina extends Component {
                     </div>
                   </div>
 
-                  <div className="col s12 m6 l6 xl3">
+                  <div className="col s12 m6 l6 xl6">
                     <div className="card gradient-45deg-amber-amber gradient-shadow min-height-100 white-text animate fadeRight">
                       <div className="padding-4">
                         <div className="row">
@@ -482,7 +482,7 @@ export default class Oficina extends Component {
                     </div>
                   </div>
 
-                  <div className="col s12 m6 l6 xl3">
+                  <div className="col s12 m6 l6 xl6">
                     <div className="card gradient-45deg-red-pink gradient-shadow min-height-100 white-text animate fadeLeft">
                       <div className="padding-4">
                         <div className="row">
@@ -492,7 +492,7 @@ export default class Oficina extends Component {
                             </i>
                           </div>
                           <div className="col s8 m8 right-align mb-7">
-                            <h5 className="mb-0 white-text">$ {(this.state.withdrawableInfinity).toFixed(3)}</h5>
+                            <h5 className="mb-0 white-text">$ {(this.state.withdrawableInfinity).toFixed(8)}</h5>
                             <p className="no-margin">Infinity ∞</p>
                           </div>
                           <div className="col s12 m12">
@@ -508,7 +508,7 @@ export default class Oficina extends Component {
                     </div>
                   </div>
 
-                  <div className="col s12 m6 l6 xl3">
+                  <div className="col s12 m6 l6 xl6">
                     <div className="card gradient-45deg-green-teal gradient-shadow min-height-100 white-text animate fadeRight">
                       <div className="padding-4">
                         <div className="row">
