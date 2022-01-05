47baf257d0dd499e8022f1a2b5a98c29484bcbce
miguelmerchan22 <miguelmerchan@hotmail.fr>
2022-01-04 18:11:59 -0600

    test crazy

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.818df5b77429e6637c5aeb37f6f75415.js"
+  "/precache-manifest.5c43ba5649795c4f68d67becb474a73c.js"
 );
 
 self.addEventListener('message', (event) => {
