47b5f4a651252e52c24a136b0f0e5d5a5af59f43
miguelmerchan22 <miguelmerchan@hotmail.fr>
2022-01-10 14:45:58 -0600

    top

---
+++ docs/demo/precache-manifest.86bee4d38dd1f7426b1574acc0c9c0cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7347881fbd0c8e8b0dffdb1b7f3c214",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "6aefe8fa56e31d51207e",
+    "url": "/demo/static/js/2.f571b379.chunk.js"
+  },
+  {
+    "revision": "4d92265ee4745234cbf941e19ca6c02e",
+    "url": "/demo/static/js/2.f571b379.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46ff40ad21f816450809",
+    "url": "/demo/static/js/main.0a4b5d88.chunk.js"
+  },
+  {
+    "revision": "8782decb24c07f68a240",
+    "url": "/demo/static/js/runtime-main.185d08bc.js"
+  }
+]);
