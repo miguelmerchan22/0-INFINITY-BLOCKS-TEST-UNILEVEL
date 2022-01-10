035b7af45d096def0843a3e9f88d70b87de6e47c
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2022-01-10 14:06:08 -0500

    update

---
+++ docs/precache-manifest.959960540e5bca2d2f04c79935f742de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f574b957d2c42c2c5633a4943be217",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "f89681cd8e5a4c6a275a",
+    "url": "/demo/static/js/2.2ab3da33.chunk.js"
+  },
+  {
+    "revision": "4d92265ee4745234cbf941e19ca6c02e",
+    "url": "/demo/static/js/2.2ab3da33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a16a7d7ca19cb4063984",
+    "url": "/demo/static/js/main.e3d0f0bb.chunk.js"
+  },
+  {
+    "revision": "1b28891bea4de072260c",
+    "url": "/demo/static/js/runtime-main.3b8c64bb.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/demo/precache-manifest.2766d0215de060d19bb58f7e9d570b65.js"
+  "/demo/precache-manifest.959960540e5bca2d2f04c79935f742de.js"
 );
 
 self.addEventListener('message', (event) => {
